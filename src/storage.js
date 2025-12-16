4faa7b79df8cb1797a20b314d0916d01feb4711a
lol123ivan777 <idiidmmme@gmail.com>
2025-12-17 02:25:33 +0300

    Update storage.js

--- src/storage.js
+++ src/storage.js
@@ -1,9 +1,1 @@
-const HI_KEY = "neon_hi";
-
-export function getHiScore() {
-  return Number(localStorage.getItem(HI_KEY) || "0");
-}
-
-export function setHiScore(score) {
-  localStorage.setItem(HI_KEY, String(score));
-}
+.
