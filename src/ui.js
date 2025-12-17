eb2a2d51ac8dc6d06f8e971490e9548bfc1dbc4f
lol123ivan777 <idiidmmme@gmail.com>
2025-12-17 10:23:52 +0300

    Update ui.js

--- src/ui.js
+++ src/ui.js
@@ -1,1 +1,14 @@
-.
+import { CONFIG } from "./config.js";
+
+export function createUI(scene, state) {
+  return {
+    score: scene.add.text(16, 16, "0", {
+      fontSize: "24px",
+      color: CONFIG.colors.text
+    })
+  };
+}
+
+export function updateUI(state) {
+  state.ui.score.setText(state.score);
+}
