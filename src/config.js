6abb2bfda81494ab8717340a15a2a41795db0af8
lol123ivan777 <idiidmmme@gmail.com>
2025-12-17 10:22:10 +0300

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,1 +1,25 @@
-.
+export const CONFIG = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+
+  lanes: 4,
+
+  colors: {
+    bg: 0x0b0014,
+    road: 0x140016,
+    neon: 0xff2b8f,
+    text: "#ffffff"
+  },
+
+  player: {
+    yOffset: 120,
+    emoji: "🚗"
+  },
+
+  items: [
+    { type: "bad", icon: "💩", score: -100, weight: 30 },
+    { type: "bad", icon: "👻", score: -100, weight: 20 },
+    { type: "good", icon: "🍒", score: 100, weight: 25 },
+    { type: "good", icon: "🍓", score: 100, weight: 25 }
+  ]
+};
