0bdb4bcdf94fd74e8db3c0c538016500d6ccf5dc
lol123ivan777 <idiidmmme@gmail.com>
2025-12-17 10:23:13 +0300

    Update player.js

--- src/player.js
+++ src/player.js
@@ -1,1 +1,23 @@
-.
+.import { CONFIG } from "./config.js";
+
+export function createPlayer(scene, state) {
+  const y = CONFIG.height - CONFIG.player.yOffset;
+
+  const player = scene.add.text(
+    state.lanesX[state.lane],
+    y,
+    CONFIG.player.emoji,
+    { fontSize: "42px" }
+  ).setOrigin(0.5);
+
+  return player;
+}
+
+export function movePlayer(state, dir) {
+  state.lane = Phaser.Math.Clamp(
+    state.lane + dir,
+    0,
+    CONFIG.lanes - 1
+  );
+  state.player.x = state.lanesX[state.lane];
+}
