bd5de4e560398675d0cafaa9735b469472e0674a
lol123ivan777 <idiidmmme@gmail.com>
2025-12-17 10:34:58 +0300

    Update ui.js

--- src/ui.js
+++ src/ui.js
@@ -1,14 +1,21 @@
-import { CONFIG } from "./config.js";
-
 export function createUI(scene, state) {
-  return {
-    score: scene.add.text(16, 16, "0", {
-      fontSize: "24px",
-      color: CONFIG.colors.text
-    })
-  };
+  const scoreText = scene.add.text(
+    16,
+    16,
+    "0",
+    { fontSize: "24px", color: "#ffffff" }
+  );
+
+  const hint = scene.add.text(
+    scene.scale.width / 2,
+    scene.scale.height / 2,
+    "ТАП — СТАРТ\nСВАЙП ← →",
+    { fontSize: "18px", color: "#ffffff", align: "center" }
+  ).setOrigin(0.5);
+
+  return { scoreText, hint };
 }
 
 export function updateUI(state) {
-  state.ui.score.setText(state.score);
+  state.ui.scoreText.setText(state.score);
 }
